5719e9c8274e6d959108975bce7c9f7817b44ad7
beanjuice <rotello.caleb@gmail.com>
2019-11-09 19:49:45 -0700

    added newlines to data parsing

--- generate-text/generate-text.py
+++ generate-text/generate-text.py
@@ -3,12 +3,16 @@ from textgenrnn import textgenrnn
 outDir = "outputs"
 inDir = "resource"
 
+textgen = textgenrnn()
+textgen.train_from_file('resource/built.txt')
+textgen.generate()
+'''
 def generate_text(fileInput, fileOutput):
     textgen = textgenrnn()
     # train the model
-    textgen.train_from_file(fileInput)#, new_model=True, max_length=3, num_epochs=2, gen_epochs=1, word_level=True)
+#j    textgen.train_from_file(fileInput)#, new_model=True, max_length=3, num_epochs=2, gen_epochs=1, word_level=True)
     textgen.train_from_file('resource/built.txt')
-#    textgen.generate()
+    textgen.generate()
 #    textgen.generate_to_file('outputs/built.txt')
     # generate text from the model
 #    textgen.generate_to_file(fileOutput, n=200)
@@ -16,6 +20,7 @@ def generate_text(fileInput, fileOutput):
 
 
 #generate_text(inDir+"/accomplishments.txt", outDir+"/accomplishments.txt")
+'''
 '''
 generate_text(inDir+"/built.txt", outDir+"/built.txt")
 generate_text(inDir+"/challenges.txt", outDir+"/challenges.txt")
